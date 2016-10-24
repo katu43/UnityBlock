06decd5ea79e2412be3e65e2bf646b655944c6c6
katu432000 <katu432007@yahoo.co.jp>
2016-10-24 22:46:47 +0900

    Stage_Block_Create
    ステージとブロックの配置まで

---
+++ UnityBlock/Assets/Script/MouseTouch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseTouch : MonoBehaviour {
+
+	public void Update(){
+		PrintClickPos ();
+	}
+
+	// タッチの位置を表示
+	void PrintClickPos(){
+		if (Input.GetMouseButtonDown (0)) {
+			Vector2 tapPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Debug.Log ("X: " + tapPoint.x + " Y: " + tapPoint.y);
+		}
+	}
+
+}
